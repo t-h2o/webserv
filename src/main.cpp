5a767e2980d5102c121104983c99f89d80f90582
To grivel <grivel.theo@protonmail.com>
2023-02-20 21:22:32 +0100

    main: check number arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
+#include "arguments.hpp"
 #include "f.hpp"
 #include <iostream>
 
 int
-main(void)
+main(int argc, char **argv)
 {
+	(void)argv;
+
+	if (check_argc_number(argc))
+		return 1;
+
 	std::cout << "C++ template" << std::endl;
 	f();
 	return 0;
