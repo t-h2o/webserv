ec6056de6e4b65ab56cdc259d488d26bf737805e
|°_°| <grivel.theo@protonmail.com>
2023-02-23 14:19:02 +0100

    created a usage message #9
    This message is printed when the user doesn't put 1 argument.
    I created this message from grep message:
    Usage: webserv [CONFIGURATION_FILE]

--- src/arguments/check_argc_number.cpp
+++ src/arguments/check_argc_number.cpp
@@ -1,7 +1,17 @@
+#include <iostream>
+
+static void
+print_help(void)
+{
+	std::cerr << "Usage: webserv [CONFIGURATION_FILE]" << std::endl;
+}
+
 int
 check_argc_number(int argc)
 {
 	if (argc == 2)
 		return 0;
+
+	print_help();
 	return 1;
 }
