199b2cfdb392611495fbd4ab56687e9a8cdd19b9
Theo Grivel <tgrivel@c2r4s4.42lausanne.ch>
2023-02-24 02:34:08 +0100

    webserver: catch Json error

--- src/webserver.cpp
+++ src/webserver.cpp
@@ -4,7 +4,20 @@
 int
 webserver(char const *path)
 {
-	Config *config = Json::read(path);
+	Config *config(0);
+
+	try
+	{
+		config = Json::read(path);
+	}
+	catch (const std::runtime_error &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 
 	if (config == 0)
 		return 1;
