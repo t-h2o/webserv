0f3ba3252d71187600e5d758b7e7b43e934bdbbc
Theo Grivel <tgrivel@c2r4s4.42lausanne.ch>
2023-02-24 01:03:35 +0100

    tdd: json01: check streams and config *

--- test/src/Json/01_test.cpp
+++ test/src/Json/01_test.cpp
@@ -1,13 +1,44 @@
+#include "Stream.hpp"
+
 #include "Config.hpp"
 #include "Json.hpp"
+
+static int
+test_config(Config *config)
+{
+	Stream output;
+
+	output.open();
+	config->print_all();
+	output.close();
+
+	if (output.check("firstName : John\n", ""))
+		return 1;
+
+	return 0;
+}
 
 int
 test_json_normal_file(void)
 {
-	Config *config = Json::read("test/src/Json/config.json");
+	Stream	output;
+	Config *config;
+
+	output.open();
+	config = Json::read("test/src/Json/config.json");
+	output.close();
 
 	if (config == 0)
 		return 1;
+
+	if (output.check("", ""))
+		return 1;
+
+	if (test_config(config))
+	{
+		delete config;
+		return 1;
+	}
 
 	delete config;
 
