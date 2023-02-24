800b99e7f0a36fcda366b89c7df1c515f75979de
Robin Burri <robinburri@tutanota.com>
2023-02-24 13:30:59 +0100

    static-website added

---
+++ test/website/app.js
@@ -0,0 +1,36 @@
+const form = document.getElementById('form');
+const input = document.getElementById('text-input');
+const itemsList = document.getElementById('items-hook');
+
+const url = "http://localhost:8080/testpost";
+
+
+
+const submitHandler = (e) => {
+	e.preventDefault();
+	if (input.value.length === 0) return;
+	const htmlEl = createHtmlEl(input.value);
+	itemsList.appendChild(htmlEl);
+	sendHttpRequest('POST', url, input.value);
+	input.value = "";
+}
+
+const createHtmlEl = (str) => {
+	const div = document.createElement('div');
+	div.className = 'item-div';
+	const h2 = document.createElement('h2');
+	h2.textContent = str;
+	div.appendChild(h2);
+	return div;
+}
+
+
+form.addEventListener('click', submitHandler);
+
+const sendHttpRequest = (method, url, data) => {
+	return fetch(url, { method: method, mode: "no-cors", body: data });
+}
+
+// Quand on click sur Submit button dans le website il envoit un POST request à http://localhost:8080/testpost
+// avec pour body le text entré dans le input field. (si input field vide, il ne fait pas de request)
+// Pour l'instant il n'y a que POST request qui est implemanté.
