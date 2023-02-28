89fddf2fa823ca26e5c0e5dc22476b4d38d016c9
Theo Grivel <tgrivel@c1r15s6.42lausanne.ch>
2023-02-28 14:04:52 +0100

    Json: catch exeption of process_line into read()

--- src/Json/Json.cpp
+++ src/Json/Json.cpp
@@ -22,7 +22,20 @@ read(char const *path)
 
 	/* Read each lines up to EOF */
 	for (getline(file, line); !file.eof(); getline(file, line))
-		_process_line(config, line, states);
+	{
+		try
+		{
+			_process_line(config, line, states);
+		}
+		catch (const std::runtime_error &e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+		catch (const std::exception &e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
 
 	if (states[OBJECT])
 		throw std::runtime_error("Json: object not finished by a '}'");
