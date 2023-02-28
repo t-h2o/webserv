c4e0299dcec0f1d169c29abd6ed84fe46514e328
|°_°| <grivel.theo@protonmail.com>
2023-02-28 23:43:11 +0100

    Config: throw an exception

--- src/webserver.cpp
+++ src/webserver.cpp
@@ -11,6 +11,14 @@ webserver(const char *path)
 	config.insert_pair(std::make_pair("key", Value("value")));
 	config.insert_pair(std::make_pair("asdf", Value(42)));
 	std::cout << " number asdf: " << config.get<double>("asdf") << std::endl;
+	try
+	{
+		std::cout << " number asdf: " << config.get<double>("nodefine") << std::endl;
+	}
+	catch (std::exception const &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
 	config.print_all();
 
 	return 0;
