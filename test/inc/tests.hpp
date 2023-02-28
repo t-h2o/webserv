b1e952a9cb2603487c47cb7f57a05ed6dc8f44a9
|°_°| <grivel.theo@protonmail.com>
2023-03-01 00:39:34 +0100

    tdd: value: string test

--- test/inc/tests.hpp
+++ test/inc/tests.hpp
@@ -7,4 +7,9 @@ int arguments_launcher(void);
 int test_check_argc_number_1(void);
 int test_check_argc_number_2(void);
 
+/* Value class */
+
+int value_launcher(void);
+int test_value_string(void);
+
 #endif /* TESTS_H */
