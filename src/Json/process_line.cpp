2bdd71fb526c589d09da3dd0801bfde2aa4637c7
|°_°| <grivel.theo@protonmail.com>
2023-02-28 20:58:21 +0100

    Json: add a if (LOG)

--- src/Json/process_line.cpp
+++ src/Json/process_line.cpp
@@ -64,7 +64,8 @@ _process_line(Config *config, std::string const &line, bool states[NSTATES])
 	while (line[index])
 	{
 		_set_states(line[index], states);
-		_print_case(line, index, states, false);
+		if (LOG)
+			_print_case(line, index, states, false);
 
 		if (states[OBJECT] && states[STRING] && !states[KEY_FILLED] && !states[MIDDLE] && !states[RIGHT]
 			&& !states[VALUE_FILLED])
