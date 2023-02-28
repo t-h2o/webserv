5df85e09e5d95e66dd12b25187224acf408228de
|°_°| <grivel.theo@protonmail.com>
2023-02-28 22:54:00 +0100

    Config: map<string, Value * -> Value>

--- src/webserver.cpp
+++ src/webserver.cpp
@@ -8,8 +8,8 @@ webserver(const char *path)
 	std::cout << "Web server C++98" << std::endl;
 
 	Config config;
-	config.insert_pair(std::make_pair("key", new Value("value")));
-	config.insert_pair(std::make_pair("asdf", new Value(42)));
+	config.insert_pair(std::make_pair("key", Value("value")));
+	config.insert_pair(std::make_pair("asdf", Value(42)));
 	std::cout << " number asdf: " << config.get<double>("asdf") << std::endl;
 	config.print_all();
 
