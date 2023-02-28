9491768990820f664ec2c65e1c28c1406d5ab85b
|°_°| <grivel.theo@protonmail.com>
2023-02-28 22:40:12 +0100

    Config: templarized get()

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,8 @@ main(int argc, char **argv)
 
 	Config config;
 	config.insert_pair(std::make_pair("key", new Value("value")));
+	config.insert_pair(std::make_pair("asdf", new Value(42)));
+	std::cout << " number asdf: " << config.get<double>("asdf") << std::endl;
 	config.print_all();
 	return 0;
 }
