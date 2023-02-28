e60d42ad4a2d392dc67f42c8514afdab6123fd32
|°_°| <grivel.theo@protonmail.com>
2023-02-28 22:34:32 +0100

    Value: templarized (add double type)

--- inc/Value.hpp
+++ inc/Value.hpp
@@ -3,16 +3,23 @@
 
 #include <iostream>
 
+#define V_STRING 1
+#define V_NUMBER 2
+
 class Value
 {
   public:
 	Value(std::string const &);
+	Value(double const &);
 	~Value();
 
-	std::string get(void) const;
+	template <typename T> T get(void) const;
+
+	int get_type(void) const;
 
   private:
 	void *_value;
+	int	  _type;
 };
 
 std::ostream &operator<<(std::ostream &, Value const &);
