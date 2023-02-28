cf2235aff76b2b215115290f45dee43a962be0fc
|°_°| <grivel.theo@protonmail.com>
2023-02-28 23:43:11 +0100

    Config: throw an exception

--- inc/Config.hpp
+++ inc/Config.hpp
@@ -20,7 +20,7 @@ class Config
 	{
 		std::map<std::string, Value>::iterator it(_config.find(key));
 		if (it == _config.end())
-			throw std::exception();
+			throw std::runtime_error("Config: do not find pair with \"" + key + "\" as key");
 		return it->second.get<T>();
 	}
 
