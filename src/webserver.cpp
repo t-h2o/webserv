bdfead56c0092eec88012564d0b73df86c1f4ac6
|°_°| <grivel.theo@protonmail.com>
2023-02-28 21:03:42 +0100

    main: example of Config and value class usage

--- src/webserver.cpp
+++ src/webserver.cpp
@@ -7,5 +7,9 @@ webserver(const char *path)
 
 	std::cout << "Web server written in C++98" << std::endl;
 
+	Config config;
+	config.insert_pair(std::make_pair("key", new Value("value")));
+	config.print_all();
+
 	return 0;
 }
