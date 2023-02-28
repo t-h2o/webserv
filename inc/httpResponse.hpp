b4f69e6ea9f31bb863ca9104b2d6c9b7d48c166f
Robin Burri <robinburri@tutanota.com>
2023-02-28 13:13:45 +0100

    HttpResponse added, working in progress

---
+++ inc/httpResponse.hpp
@@ -0,0 +1,38 @@
+#ifndef HTTPRESPONSE_HPP
+#define HTTPRESPONSE_HPP
+
+#include "statusCodes.hpp"
+#include <fstream>
+#include <string>
+
+class HttpResponse
+{
+  public:
+	HttpResponse(void);
+	~HttpResponse(void);
+	std::string get_time_stamp(void);
+	int			count_file_size(std::string path);
+	void		init_response_map(void);
+	void		load_response_map(int status_code);
+	void		print_response_map(void);
+
+  private:
+	std::map<std::string, std::string> _response_map;
+	StatusCode						   _status_code;
+	// std::string _full_response;
+	// size_t _header_size;
+	// size_t _body_size;
+	// size_t _total_size;
+	// std::string _date;
+};
+
+#endif
+
+/*
+	Date: Sun, 06 Nov 1994 08:49:37 GMT
+	Server: webserver
+	Content-Length:
+	Content-Type:
+	Connection: Closed
+	Transfer-Encoding: chunked => ????
+*/
---
+++ src/httpResponse/httpResponse.cpp
@@ -0,0 +1,56 @@
+#include "httpResponse.hpp"
+
+HttpResponse::HttpResponse(void) { init_response_map(); };
+
+HttpResponse::~HttpResponse(void){};
+
+void
+HttpResponse::init_response_map(void)
+{
+	_response_map["Status-line"] = "";
+	_response_map["Date"] = "";
+	_response_map["Server"] = "Webserver";
+	_response_map["Content-Length"] = "";
+	_response_map["Content-Type"] = "";
+	_response_map["Connection"] = "";
+	_response_map["Protocol"] = "HTTP/1.1 ";
+};
+
+void
+HttpResponse::load_response_map(int status_code)
+{
+	_response_map["Date"] += get_time_stamp();
+	_response_map["Status-line"]
+		= _response_map["Protocol"] + _status_code.get_key_value_formated(status_code);
+}
+
+std::string
+HttpResponse::get_time_stamp(void)
+{
+	std::time_t stamp = std::time(NULL);
+	std::string formated_date = std::asctime(std::gmtime(&stamp));
+	formated_date = formated_date.substr(0, formated_date.length() - 1);
+	formated_date += " GMT";
+	return formated_date;
+}
+
+void
+HttpResponse::print_response_map(void)
+{
+	std::map<std::string, std::string>::iterator it;
+	for (it = _response_map.begin(); it != _response_map.end(); it++)
+	{
+		std::cout << it->first << " : " << it->second << std::endl;
+	}
+}
+
+int
+HttpResponse::count_file_size(std::string path)
+{
+	std::ifstream stream;
+	int			  size;
+	stream.open(path.c_str(), std::ios::binary);
+	stream.seekg(0, std::ios::end);
+	size = stream.tellg();
+	return size;
+}
