d723f54c7b280eb08ee6b6cee07886fdd56ba5e6
|°_°| <grivel.theo@protonmail.com>
2023-02-28 23:43:11 +0100

    Config: throw an exception

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,14 @@ main(int argc, char **argv)
 	config.insert_pair(std::make_pair("key", Value("value")));
 	config.insert_pair(std::make_pair("asdf", Value(42)));
 	std::cout << " number asdf: " << config.get<double>("asdf") << std::endl;
+	try
+	{
+		std::cout << " number asdf: " << config.get<double>("nodefine") << std::endl;
+	}
+	catch (std::exception const &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
 	config.print_all();
 	return 0;
 }
