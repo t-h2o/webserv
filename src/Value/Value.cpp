7c2473c4e39d293a7f4b44fd1fb37976a3abccc3
|°_°| <grivel.theo@protonmail.com>
2023-02-28 22:34:32 +0100

    Value: templarized (add double type)

--- src/Value/Value.cpp
+++ src/Value/Value.cpp
@@ -1,19 +1,37 @@
 #include "Value.hpp"
 
-Value::Value(std::string const &value) : _value(0) { _value = new std::string(value); }
+Value::Value(std::string const &value) : _value(0), _type(V_STRING) { _value = new std::string(value); }
+Value::Value(double const &value) : _value(0), _type(V_NUMBER) { _value = new double(value); }
 
-Value::~Value(void) { delete static_cast<std::string *>(_value); }
+Value::~Value(void)
+{
 
-std::string
+	if (_type == V_STRING)
+		delete static_cast<std::string *>(_value);
+	else if (_type == V_NUMBER)
+		delete static_cast<double *>(_value);
+}
+
+template <typename T>
+T
 Value::get(void) const
 {
-	return *static_cast<std::string *>(_value);
+	return *static_cast<T *>(_value);
+}
+
+int
+Value::get_type(void) const
+{
+	return _type;
 }
 
 std::ostream &
 operator<<(std::ostream &output, Value const &value)
 {
-	output << value.get();
+	if (value.get_type() == V_STRING)
+		output << value.get<std::string>();
+	else if (value.get_type() == V_NUMBER)
+		output << value.get<double>();
 
 	return output;
 }
