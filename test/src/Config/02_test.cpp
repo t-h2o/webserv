61a79d59ac410cb39f725a46a7a77ab7842e6133
|°_°| <grivel.theo@protonmail.com>
2023-03-01 16:02:26 +0100

    tdd: config class: double type

--- test/src/Config/02_test.cpp
+++ test/src/Config/02_test.cpp
@@ -7,10 +7,16 @@ test_config_get(void)
 
 	config.insert_pair(std::make_pair("key1", Value("value1")));
 	config.insert_pair(std::make_pair("key2", Value("value2")));
+	config.insert_pair(std::make_pair("key3", Value(42)));
+	config.insert_pair(std::make_pair("key4", Value(1234.5678)));
 
 	if (config.get<std::string>("key1") != "value1")
 		return 1;
 	if (config.get<std::string>("key2") != "value2")
+		return 1;
+	if (config.get<double>("key3") != 42)
+		return 1;
+	if (config.get<double>("key4") != 1234.5678)
 		return 1;
 	{
 		Config config_copy(config);
@@ -18,6 +24,10 @@ test_config_get(void)
 		if (config_copy.get<std::string>("key1") != "value1")
 			return 1;
 		if (config_copy.get<std::string>("key2") != "value2")
+			return 1;
+		if (config_copy.get<double>("key3") != 42)
+			return 1;
+		if (config_copy.get<double>("key4") != 1234.5678)
 			return 1;
 	}
 
@@ -27,6 +37,10 @@ test_config_get(void)
 		if (config_copy.get<std::string>("key1") != "value1")
 			return 1;
 		if (config_copy.get<std::string>("key2") != "value2")
+			return 1;
+		if (config_copy.get<double>("key3") != 42)
+			return 1;
+		if (config_copy.get<double>("key4") != 1234.5678)
 			return 1;
 	}
 
