9286c315f7492c6870b9d3e64690d62375dd171a
|°_°| <grivel.theo@protonmail.com>
2023-03-01 15:37:21 +0100

    main.cpp: clean the file

--- src/webserver.cpp
+++ src/webserver.cpp
@@ -6,21 +6,7 @@ webserver(const char *path)
 {
 	(void)path;
 
-	std::cout << "Web server C++98" << std::endl;
-
-	Config config;
-	config.insert_pair(std::make_pair("key", Value("value")));
-	config.insert_pair(std::make_pair("asdf", Value(42)));
-	std::cout << " number asdf: " << config.get<double>("asdf") << std::endl;
-	try
-	{
-		std::cout << " number asdf: " << config.get<double>("nodefine") << std::endl;
-	}
-	catch (std::exception const &e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
-	config.print_all();
+	std::cout << "Web server written in C++98" << std::endl;
 
 	return 0;
 }
