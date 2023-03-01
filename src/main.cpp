c888010c84f39db3163ba6f8a198a5bb3d687597
|°_°| <grivel.theo@protonmail.com>
2023-03-01 15:37:21 +0100

    main.cpp: clean the file

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include "Config.hpp"
 #include "arguments.hpp"
 #include <iostream>
 
@@ -12,18 +11,5 @@ main(int argc, char **argv)
 
 	std::cout << "Web server C++98" << std::endl;
 
-	Config config;
-	config.insert_pair(std::make_pair("key", Value("value")));
-	config.insert_pair(std::make_pair("asdf", Value(42)));
-	std::cout << " number asdf: " << config.get<double>("asdf") << std::endl;
-	try
-	{
-		std::cout << " number asdf: " << config.get<double>("nodefine") << std::endl;
-	}
-	catch (std::exception const &e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
-	config.print_all();
 	return 0;
 }
