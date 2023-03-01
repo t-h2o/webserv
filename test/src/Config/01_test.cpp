45ccf2d2f3959a0afab7ab08ed11ea0af036dc7a
|°_°| <grivel.theo@protonmail.com>
2023-03-01 16:02:26 +0100

    tdd: config class: double type

--- test/src/Config/01_test.cpp
+++ test/src/Config/01_test.cpp
@@ -7,6 +7,8 @@ test_config_insert(void)
 
 	config.insert_pair(std::make_pair("key1", Value("value1")));
 	config.insert_pair(std::make_pair("key2", Value("value2")));
+	config.insert_pair(std::make_pair("key3", Value(42)));
+	config.insert_pair(std::make_pair("key4", Value(1234.5678)));
 
 	{
 		Config config_copy(config);
