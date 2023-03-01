8e94de9e53dfc19964e44af7be257e1b19757dca
|°_°| <grivel.theo@protonmail.com>
2023-03-01 15:31:46 +0100

    tdd: config class: copy constructor

--- test/src/Config/01_test.cpp
+++ test/src/Config/01_test.cpp
@@ -8,5 +8,13 @@ test_config_insert(void)
 	config.insert_pair(std::make_pair("key1", Value("value1")));
 	config.insert_pair(std::make_pair("key2", Value("value2")));
 
+	{
+		Config config_copy(config);
+	}
+
+	{
+		Config config_copy = config;
+	}
+
 	return 0;
 }
