e4028c9d23f779652b8e5b29df3025faef27d04d
|°_°| <grivel.theo@protonmail.com>
2023-03-02 10:17:37 +0100

    tdd: Json: number in config

--- test/inc/tests.hpp
+++ test/inc/tests.hpp
@@ -20,6 +20,7 @@ int test_json_normal_file(void);
 int test_json_bad_path(void);
 int test_json_empty_string(void);
 int test_json_missing_brace(void);
+int test_json_number(void);
 
 /* Value class */
 
--- test/src/Json/00_launcher.cpp
+++ test/src/Json/00_launcher.cpp
@@ -10,5 +10,6 @@ json_launcher(void)
 	list.load_test("bad path", &test_json_bad_path);
 	list.load_test("exception: empty string", &test_json_empty_string);
 	list.load_test("exception: missing brace", &test_json_missing_brace);
+	list.load_test("normal JSON number", &test_json_number);
 	return (list.launch_tests());
 }
