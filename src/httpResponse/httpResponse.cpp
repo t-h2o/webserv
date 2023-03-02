eef659a38297b85fc4f4982e3b5d29c7ab05c5c5
Robin Burri <robinburri@tutanota.com>
2023-03-02 12:35:26 +0100

    created construct_header_string()

--- src/httpResponse/httpResponse.cpp
+++ src/httpResponse/httpResponse.cpp
@@ -4,8 +4,9 @@ HttpResponse::HttpResponse(void){};
 
 HttpResponse::~HttpResponse(void){};
 
-void HttpResponse::init_response_map(void)
-{ 
+void
+HttpResponse::init_response_map(void)
+{
 	_response_map["Status-line"] = "";
 	_response_map["Date"] = "";
 	_response_map["Server"] = "Webserver";
@@ -13,20 +14,23 @@ void HttpResponse::init_response_map(void)
 	_response_map["Content-Type"] = "";
 	_response_map["Connection"] = "close";
 	_response_map["Protocol"] = "HTTP/1.1 ";
-	_response_map["header-string"] =  "";
+	_response_map["header-string"] = "";
 	_response_map["body-string"] = "";
 	_response_map["full-reponse-string"] = "";
 	_response_map["dir_location"] = "/Users/rburri/Desktop/network_cpp/server_lib";
 };
 
-void HttpResponse::load_response_map(int status_code)
+void
+HttpResponse::load_response_map(int status_code)
 {
 	_response_map["Date"] += get_time_stamp();
-	_response_map["Status-line"] = _response_map["Protocol"] + _status_code.get_key_value_formated(status_code);
+	_response_map["Status-line"]
+		= _response_map["Protocol"] + _status_code.get_key_value_formated(status_code);
 	_response_map["dir_location"] += _request_path;
 }
 
-std::string HttpResponse::get_time_stamp(void)
+std::string
+HttpResponse::get_time_stamp(void)
 {
 	std::time_t stamp = std::time(NULL);
 	std::string formated_date = std::asctime(std::gmtime(&stamp));
@@ -35,8 +39,8 @@ std::string HttpResponse::get_time_stamp(void)
 	return formated_date;
 }
 
-
-void HttpResponse::count_file_size(std::string path)
+void
+HttpResponse::count_file_size(std::string path)
 {
 	std::ifstream stream;
 	if (file_exists(path))
@@ -49,10 +53,11 @@ void HttpResponse::count_file_size(std::string path)
 	}
 }
 
-bool HttpResponse::file_exists(std::string path)
+bool
+HttpResponse::file_exists(std::string path)
 {
 	std::ifstream file;
-	bool ret = false;
+	bool		  ret = false;
 	file.open(path.c_str());
 	if (file)
 	{
@@ -62,7 +67,8 @@ bool HttpResponse::file_exists(std::string path)
 	return ret;
 }
 
-void HttpResponse::set_response_type(std::string path, std::string type = "")
+void
+HttpResponse::set_response_type(std::string path, std::string type = "")
 {
 	if (type != "")
 	{
@@ -85,7 +91,8 @@ void HttpResponse::set_response_type(std::string path, std::string type = "")
 		_response_map["Content-Type"] = "text/plain";
 }
 
-void HttpResponse::print_response_map(void)
+void
+HttpResponse::print_response_map(void)
 {
 	std::map<std::string, std::string>::iterator it;
 	for (it = _response_map.begin(); it != _response_map.end(); it++)
@@ -94,19 +101,22 @@ void HttpResponse::print_response_map(void)
 	}
 }
 
-void HttpResponse::load_http_request(HttpRequest &req)
+void
+HttpResponse::load_http_request(HttpRequest &req)
 {
 	_request_path = req.getPath();
-	std::cout << " Request Path: "<<_request_path << std::endl;
+	std::cout << " Request Path: " << _request_path << std::endl;
 	response_handler();
 }
 
-void HttpResponse::load_content_length(void)
+void
+HttpResponse::load_content_length(void)
 {
 	_response_map["Content-Length"] = std::to_string(_body_size);
 }
 
-void HttpResponse::response_handler()
+void
+HttpResponse::response_handler(void)
 {
 	init_response_map();
 	// CHECKER HERE
@@ -115,5 +125,25 @@ void HttpResponse::response_handler()
 	load_response_map(200);
 	count_file_size(_response_map["dir_location"]);
 	load_content_length();
+	construct_header_string();
 }
 
+void
+HttpResponse::construct_header_string(void)
+{
+	std::string CRLF = "\r\n";
+
+	_response_map["header-string"] += _response_map["Status-line"];
+	_response_map["header-string"] += CRLF;
+	_response_map["header-string"] += "Date: " + _response_map["Date"];
+	_response_map["header-string"] += CRLF;
+	_response_map["header-string"] += "Server: " + _response_map["Server"];
+	_response_map["header-string"] += CRLF;
+	_response_map["header-string"] += "Content-Length: " + _response_map["Content-Length"];
+	_response_map["header-string"] += CRLF;
+	_response_map["header-string"] += "Content-Type: " + _response_map["Content-Type"];
+	_response_map["header-string"] += CRLF;
+	_response_map["header-string"] += "Connection: " + _response_map["Connection"];
+	_response_map["header-string"] += CRLF;
+	_response_map["header-string"] += CRLF;
+}
