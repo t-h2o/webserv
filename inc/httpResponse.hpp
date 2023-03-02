ea3abfc6d5b915ac2ad38e7e4934d882268b5abe
Robin Burri <robinburri@tutanota.com>
2023-03-02 12:34:07 +0100

    maximum of member functions in private

--- inc/httpResponse.hpp
+++ inc/httpResponse.hpp
@@ -9,44 +9,27 @@
 
 class HttpResponse
 {
-public:
+  public:
 	HttpResponse();
 	~HttpResponse(void);
-	std::string get_time_stamp(void);
-	void count_file_size(std::string path);
-	void init_response_map(void);
-	void load_response_map(int status_code);
 	void load_http_request(HttpRequest &req);
-	void load_content_length(void);
 	void print_response_map(void);
-	bool file_exists(std::string path);
-	void set_response_type(std::string path, std::string type);
-	void response_handler(void);
+	void init_response_map(void);
 
-		private : std::map<std::string, std::string>
-					  _response_map;
-	StatusCode _status_code;
-	HttpRequest _request;
-	std::string _request_path;
-	// std::string _full_response;
-	// size_t _header_size;
-	int _body_size;
-	// size_t _total_size;
-	// std::string _date;
+  private:
+	std::map<std::string, std::string> _response_map;
+	StatusCode						   _status_code;
+	HttpRequest						   _request;
+	std::string						   _request_path;
+	int								   _body_size;
 
+	void		response_handler(void);
+	void		set_response_type(std::string path, std::string type);
+	bool		file_exists(std::string path);
+	void		load_content_length(void);
+	void		load_response_map(int status_code);
+	std::string get_time_stamp(void);
+	void		count_file_size(std::string path);
 };
 
 #endif
-
-/*
-	Date: Sun, 06 Nov 1994 08:49:37 GMT
-	Server: webserver
-	Content-Length:
-	Content-Type:
-	Connection: Closed
-	Transfer-Encoding: chunked => ????
-*/
-
-/**
- * TODO => construct header string 
- */
