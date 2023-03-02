2963d290a738ca37bf77bbd6cdb77af7c51ccc16
|°_°| <grivel.theo@protonmail.com>
2023-03-02 15:10:18 +0100

    Value: take a pointer on vector

--- test/src/Value/03_test.cpp
+++ test/src/Value/03_test.cpp
@@ -5,24 +5,26 @@
 int
 test_value_array_number(void)
 {
-	std::vector<double> vec_double;
+	std::vector<double> *vec_double;
 
-	vec_double.push_back(1);
-	vec_double.push_back(2);
-	vec_double.push_back(3);
+	vec_double = new std::vector<double>;
+
+	vec_double->push_back(1);
+	vec_double->push_back(2);
+	vec_double->push_back(3);
 
 	Value val1(vec_double);
 
-	if (val1[0] != vec_double[0])
+	if (val1[0] != (*vec_double)[0])
 		return 1;
 
-	if (val1[1] != vec_double[1])
+	if (val1[1] != (*vec_double)[1])
 		return 1;
 
-	if (val1[2] != vec_double[2])
+	if (val1[2] != (*vec_double)[2])
 		return 1;
 
-	if (val1.size() != vec_double.size())
+	if (val1.size() != vec_double->size())
 		return 1;
 
 	return 0;
