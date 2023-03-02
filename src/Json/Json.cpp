431cf712f02e4385743aecf832968ac898724f44
|°_°| <grivel.theo@protonmail.com>
2023-03-02 10:23:18 +0100

    Json: split read function: open_file()

--- src/Json/Json.cpp
+++ src/Json/Json.cpp
@@ -1,5 +1,19 @@
 #include "utils_json.hpp"
 
+static int
+open_file(std::fstream &file, char const *path)
+{
+	file.open(path, std::fstream::in);
+
+	/* Check if the file is valid */
+	if (file.is_open() == 0)
+	{
+		std::cerr << "Error: read(): file \"" << path << "\" is not valid" << std::endl;
+		return 1;
+	}
+	return 0;
+}
+
 Config *
 read(char const *path)
 {
@@ -9,14 +23,10 @@ read(char const *path)
 	std::string	 line;
 
 	memset(states, 0, sizeof(bool) * NSTATES);
-	file.open(path, std::fstream::in);
 
-	/* Check if the file is valid */
-	if (file.is_open() == 0)
-	{
-		std::cerr << "Error: read(): file \"" << path << "\" is not valid" << std::endl;
+	/* open the file */
+	if (open_file(file, path))
 		return 0;
-	}
 
 	config = new Config;
 
