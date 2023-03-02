712cf9ca66c899a0300c65da6f23e61347258165
Theo Grivel <tgrivel@c1r15s6.42lausanne.ch>
2023-03-02 12:39:42 +0100

    tdd: json: wip array of number

---
+++ test/src/Json/07_test.cpp
@@ -0,0 +1,46 @@
+#include "Stream.hpp"
+
+#include "Config.hpp"
+#include "Json.hpp"
+
+static int
+test_config(Config *config)
+{
+	Stream output;
+
+	output.open();
+	config->print_all();
+	output.close();
+
+	if (output.check("age : 42\nfirstName : John\n", ""))
+		return 1;
+
+	return 0;
+}
+
+int
+test_json_string_number(void)
+{
+	Stream	output;
+	Config *config;
+
+	output.open();
+	config = read("test/src/Json/config_string_number.json");
+	output.close();
+
+	if (config == 0)
+		return 1;
+
+	if (output.check("", ""))
+		return 1;
+
+	if (test_config(config))
+	{
+		delete config;
+		return 1;
+	}
+
+	delete config;
+
+	return 0;
+}
