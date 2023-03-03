7ae962544a1da10fd5038b1715adb47e55469afb
Theo Grivel <tgrivel@c1r15s5.42lausanne.ch>
2023-03-03 11:51:20 +0100

    tdd: use t_object

--- test/src/Value/05_test.cpp
+++ test/src/Value/05_test.cpp
@@ -3,11 +3,11 @@
 int
 test_value_map_value(void)
 {
-	std::map<std::string, Value> *config;
-	std::map<std::string, Value> *object;
+	Value::t_object *config;
+	Value::t_object *object;
 
-	config = new std::map<std::string, Value>;
-	object = new std::map<std::string, Value>;
+	config = new Value::t_object;
+	object = new Value::t_object;
 
 	config->insert(std::make_pair("key1", Value(new std::string("value1"))));
 	object->insert(std::make_pair("key2", Value(new std::string("value2"))));
