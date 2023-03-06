f729530b5b3c917576d0437d2cf33f30c344d74e
Theo Grivel <tgrivel@c1r15s7.42lausanne.ch>
2023-03-06 20:10:03 +0100

    tdd: test array and object in an object

--- test/src/Value/00_launcher.cpp
+++ test/src/Value/00_launcher.cpp
@@ -11,5 +11,6 @@ value_launcher(void)
 	list.load_test("test boolean", &test_value_boolean);
 	list.load_test("test array of numbers", &test_value_array_number);
 	list.load_test("test map value", &test_value_map_value);
+	list.load_test("test object and array", &test_value_object_array);
 	return (list.launch_tests());
 }
