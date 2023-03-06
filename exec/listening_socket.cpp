a2d2bd5dc145c168b3e8af7a7c7ab52f2101bbee
hack10101101 <kevin.dinocera@protonmail.com>
2023-03-06 13:41:48 +0100

    ecouter

--- exec/listening_socket.cpp
+++ exec/listening_socket.cpp
@@ -3,13 +3,53 @@
 //
 
 #include "listening_socket.hpp"
+#include <errno.h>
+typedef int SOCKET;
+//#define inet_pton(FAMILY, IP, PTR_STRUCT_SOCKADDR) (*(PTR_STRUCT_SOCKADDR)) = inet_addr((IP))
+//typedef unsigned int socklen_t;
+//#include <sys/socket.h>
+//#include <netinet/in.h> // sockaddr_in, IPPROTO_TCP
+//#include <arpa/inet.h> // hton*, ntoh*, inet_addr
+//#include <unistd.h>  // close
+//#include <cerrno> // errno
+//#define SOCKET int
+//#define INVALID_SOCKET ((int)-1)
+//bool closesocket(SOCKET socket);
 
 int main(int ac, char **av)
 {
-	sockaddr_in addr;
-	Socket sokt(socket(AF_UNIX, SOCK_STREAM, 0));
+	if (ac != 3)
+		return (1);
 
-	sokt.get_new_s();
-	sokt.bind_it(&addr);
+	char		*IP = av[1];
+	int			port = std::atoi(av[2]);
+	SOCKET		newSocket;
+	std::string clientAdresse;
+
+//	sockaddr_in server;
+//	server.sin_addr.s_addr = inet_addr(IP);
+//	server.sin_family = AF_INET;
+//	server.sin_port = port;
+
+	Socket 		sokt(port);
+
+//	int error = errno;
+
+	std::cout << "IP = " << IP << " port = " << port << std::endl;
+
+	while (1)
+	{
+		// Accepte une connexion entrante.
+		if ((newSocket = sokt.accept_it()) == -1)
+			break ;
+		else
+		{
+			// Permet de récupérer l'adresse IP d'un socket, IPv4 ou IPv6, sous forme lisible.
+			clientAdresse = sokt.IP_recept();
+			std::cout << "client addresse = " << clientAdresse << ": " << sokt.get_struct().sin_port << std::endl;
+		}
+	}
+	// Ferme le socket.
+	sokt.close_skt();
 	return (0);
 }
--- exec/listening_socket.hpp
+++ exec/listening_socket.hpp
@@ -8,36 +8,87 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
+#include <arpa/inet.h>
+
+typedef int SOCKET;
 
 class Socket
 {
 private:
-	int _new_s;
-	int _bind_it;
+	SOCKET 	_s;
+	int		_bind_it;
+	int		_listen_it;
+	int 	_its_close;
+	struct sockaddr_in _address;
 
 public:
-	Socket(void) : _new_s(0), _bind_it(0) {}
-	Socket(const int& socket_fct)
-	: _new_s(socket_fct), _bind_it(0) {}
 
-	void	bind_it(sockaddr_in *addr) {
-		addr->sin_addr.s_addr = INADDR_ANY; // indique que toutes les sources seront acceptées
-		addr->sin_port = htons(0); // toujours penser à traduire le port en réseau
-		addr->sin_family = AF_INET;
+	Socket(int port) : _s(0), _bind_it(0), _listen_it(0), _its_close(0) {
 
-		if ((_bind_it = bind(_new_s, (sockaddr*)&addr, sizeof(addr))) != 0)
+		_address.sin_addr.s_addr = INADDR_ANY; // indique que toutes les sources seront acceptées
+		_address.sin_port = htons(port); // toujours penser à traduire le port en réseau
+		_address.sin_family = AF_INET;
+
+		// Crée un socket avec les paramètres passés.
+		create_socket();
+//		// Connecte un socket précédemment créé au serveur passé en paramètre.
+//		is_connected(server);
+		// La fonction bind est utilisée pour assigner une adresse locale à un socket.
+		bind_it();
+		// Place le socket dans un état lui permettant d'écouter les connexions entrantes.
+		listen_it();
+	}
+
+	void	create_socket(void) {
+		if ((_s = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+			std::cout << _s << " socket creation error\n";
+		std::cout << "socket creation: " << _s << std::endl;
+	}
+
+//	void	is_connected(sockaddr_in *server) {
+//		if (connect(_s, (const struct sockaddr*)&server, sizeof (server)) != 0)
+//			std::cout << "connect fail\n";
+//	}
+
+	void	bind_it(void) {
+		if ((_bind_it = bind(_s, (struct sockaddr *)&_address, sizeof(_address))) != 0)
 			std::cout << _bind_it << " bad bind access\n";
 	}
 
-	void	get_new_s(void) {
-		std::cout << _new_s << std::endl;
+	void	listen_it(void) {
+		if ((_listen_it = listen(_s, SOMAXCONN)) != 0)
+			std::cout << _listen_it << " bad listen access\n";
+		std::cout << "listen: " << _listen_it << std::endl;
+	}
+
+	int	accept_it(void) {
+		socklen_t len = sizeof(_address);
+		return (accept(_s, (struct sockaddr *)&_address, &len));
+	}
+
+	const char	*IP_recept() {
+		char IP_c[INET6_ADDRSTRLEN];
+		return (inet_ntop(_address.sin_family, (void*)&(_address.sin_addr), IP_c, INET6_ADDRSTRLEN));
+	}
+
+	void	close_skt(void) {
+		if ((_its_close = close(_s))  != 0)
+			std::cout << _its_close << " close fail\n";
+	}
+
+	void	get_s(void) {
+		std::cout << _s << std::endl;
 	}
 	~Socket(void) {}
 
+	struct sockaddr_in get_struct(void) {
+		return (_address);
+	}
 //	class myexception: public exception
 //	{
 //		virtual const char* what() const throw()
