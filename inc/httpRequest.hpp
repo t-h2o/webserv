31bc07c8f0de72040ec2bf1fb15499789dcb812c
Robin Burri <robinburri@tutanota.com>
2023-03-06 13:33:26 +0100

    parsing request and send response ok, without error checking

--- inc/httpRequest.hpp
+++ inc/httpRequest.hpp
@@ -9,24 +9,24 @@
 
 class HttpRequest
 {
-  private:
-	std::map<std::string, std::string> _request_map;
-	void							   parseFirstLine(std::string firstLine);
-	void							   parseOtherLines(std::vector<std::string> tmp_vector);
-
   public:
 	HttpRequest();
-	~HttpRequest();
+	virtual ~HttpRequest();
 
-	bool		methodIsAuthorized(std::string method) const;
 	void		parseBuffer(char *buff);
-	std::string trim(const std::string &s);
 	void		printHttpReq();
 	// Getters
 	std::string getMethod() const;
 	std::string getPath() const;
 	std::string getProtocol() const;
 	std::string getHost() const;
+
+  private:
+	bool		methodIsAuthorized(std::string method) const;
+	std::string trim(const std::string &s);
+	std::map<std::string, std::string> _request_map;
+	void							   parseFirstLine(std::string firstLine);
+	void							   parseOtherLines(std::vector<std::string> tmp_vector);
 };
 
 #endif
