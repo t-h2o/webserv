c90698e140531e751b8ebfd09faabc5bec92be7c
Theo Grivel <tgrivel@c1r15s7.42lausanne.ch>
2023-03-06 18:13:07 +0100

    Value: refactor array

--- test/src/Value/03_test.cpp
+++ test/src/Value/03_test.cpp
@@ -5,26 +5,26 @@
 int
 test_value_array_number(void)
 {
-	std::vector<double> *vec_double;
+	Value::t_array *vec_double;
 
-	vec_double = new std::vector<double>;
+	vec_double = new Value::t_array;
 
-	vec_double->push_back(1);
-	vec_double->push_back(2);
-	vec_double->push_back(3);
+	vec_double->push_back(new double(1));
+	vec_double->push_back(new double(2));
+	vec_double->push_back(new double(3));
 
 	Value val1(vec_double);
 
-	if (val1[0] != (*vec_double)[0])
+	if (val1[0].get<double>() != 1)
 		return 1;
 
-	if (val1[1] != (*vec_double)[1])
+	if (val1[1].get<double>() != 2)
 		return 1;
 
-	if (val1[2] != (*vec_double)[2])
+	if (val1[2].get<double>() != 3)
 		return 1;
 
-	if (val1.size() != vec_double->size())
+	if (val1.size() != 3)
 		return 1;
 
 	return 0;
