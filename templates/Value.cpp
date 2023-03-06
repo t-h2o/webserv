b1a3f86757db9f158fe04bb2a1e13b279491e136
Theo Grivel <tgrivel@c1r15s7.42lausanne.ch>
2023-03-06 17:35:17 +0100

    Value: refactor get(string)

--- templates/Value.cpp
+++ templates/Value.cpp
@@ -6,11 +6,3 @@ Value::get(void) const
 {
 	return *static_cast<T *>(_value);
 }
-
-/* Search the key into the map for returning the value */
-template <typename T>
-T &
-Value::get(std::string const &key) const
-{
-	return get<std::map<std::string, Value> >().find(key)->second.get<T>();
-}
