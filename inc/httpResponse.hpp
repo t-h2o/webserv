d0954c5615d79114dd0c14c432d9ab2fe4be0a4e
Robin Burri <robinburri@tutanota.com>
2023-03-13 15:27:50 +0100

    error pages have css now && some basic checks on Methods and if file exits

--- inc/httpResponse.hpp
+++ inc/httpResponse.hpp
@@ -3,36 +3,35 @@
 
 #include "statusCodes.hpp"
 #include <fstream>
-#include <sstream>
 #include <string>
 #include <time.h>
-#include "httpRequest.hpp"
+#include <sstream>
+#include "HttpRequest.hpp"
 
 class HttpResponse
 {
-  public:
+public:
 	HttpResponse();
 	virtual ~HttpResponse(void);
 	std::string get_http_response(void);
-	void		print_response_map(void);
-	void		load_http_request(HttpRequest &req);
+	void print_response_map(void);
+	void load_http_request(HttpRequest &req);
 
-  private:
-	std::map<std::string, std::string> _response_map;
-	StatusCode						   _status_code;
-	HttpRequest						   _request;
-	std::string						   _request_path;
+	private : std::map<std::string, std::string> _response_map;
+	StatusCode _status_code;
+	HttpRequest _request;
+	std::string _request_path;
 
-	void		load_content_length(std::string str);
-	void		init_response_map(void);
-	void		response_handler(void);
-	void		set_response_type(std::string path);
-	bool		file_exists(std::string path);
-	void		load_response_map(int status_code);
+	void load_content_length(std::string str);
+	void init_response_map(void);
+	void set_response_type(std::string path);
+	bool file_exists(std::string path);
+	void load_response_map(int status_code);
 	std::string get_time_stamp(void);
-	void		construct_header_string(void);
-	void		construct_body_string(std::string path_to_file);
-	void		construct_full_response(void);
+	void construct_header_string(void);
+	void construct_body_string(std::string path_to_file);
+	void construct_full_response(void);
+	void create_error_html_page(int code);
 };
 
 #endif
--- src/httpResponse/httpResponse.cpp
+++ src/httpResponse/httpResponse.cpp
@@ -4,14 +4,32 @@ HttpResponse::HttpResponse(void){};
 
 HttpResponse::~HttpResponse(void){};
 
-void HttpResponse::load_http_request(HttpRequest &req)
+void
+HttpResponse::load_http_request(HttpRequest &req)
 {
-	_request_path = req.getPath();
-	response_handler();
+	init_response_map();
+	std::string requested_path = req.getPath();
+	_response_map["dir_location"] += requested_path;
+	std::cout << "dir_location at load_HTTP_REQUEST : " << _response_map["dir_location"] << std::endl;
+	std::cout << "METHOD: " << req.getMethod() << "\nAuth: " << req.methodIsAuthorized(req.getMethod())
+			  << std::endl;
+	if (!req.methodIsAuthorized(req.getMethod()))
+	{
+		load_response_map(405);
+	}
+	else if (!file_exists(_response_map["dir_location"]))
+	{
+		load_response_map(404);
+	}
+	else
+	{
+		load_response_map(200);
+	}
 }
 
-void HttpResponse::init_response_map(void)
-{ 
+void
+HttpResponse::init_response_map(void)
+{
 	_response_map["Status-line"] = "";
 	_response_map["Date"] = "";
 	_response_map["Server"] = "Webserver";
@@ -19,25 +37,35 @@ void HttpResponse::init_response_map(void)
 	_response_map["Content-Type"] = "";
 	_response_map["Connection"] = "Closed";
 	_response_map["Protocol"] = "HTTP/1.1 ";
-	_response_map["header-string"] =  "";
+	_response_map["header-string"] = "";
 	_response_map["body-string"] = "";
 	_response_map["full-response-string"] = "";
 	_response_map["dir_location"] = "/Users/rburri/Desktop/network_cpp";
 };
 
-void HttpResponse::load_response_map(int status_code)
+void
+HttpResponse::load_response_map(int status_code)
 {
 	_response_map["Date"] += get_time_stamp();
-	_response_map["Status-line"] = _response_map["Protocol"] + _status_code.get_key_value_formated(status_code);
-	_response_map["dir_location"] += _request_path;
-	set_response_type(_request_path);
-	construct_body_string(_response_map["dir_location"]);
+	_response_map["Status-line"]
+		= _response_map["Protocol"] + _status_code.get_key_value_formated(status_code);
+	if (status_code != 200)
+	{
+		set_response_type(".html");
+		create_error_html_page(status_code);
+	}
+	else
+	{
+		set_response_type(_response_map["dir_location"]);
+		construct_body_string(_response_map["dir_location"]);
+	}
 	load_content_length(_response_map["body-string"]);
 	construct_header_string();
 	construct_full_response();
 }
 
-std::string HttpResponse::get_time_stamp(void)
+std::string
+HttpResponse::get_time_stamp(void)
 {
 	std::time_t stamp = std::time(NULL);
 	std::string formated_date = std::asctime(std::gmtime(&stamp));
@@ -46,24 +74,28 @@ std::string HttpResponse::get_time_stamp(void)
 	return formated_date;
 }
 
-void HttpResponse::load_content_length(std::string str)
+void
+HttpResponse::load_content_length(std::string str)
 {
 	_response_map["Content-Length"] = std::to_string(str.length());
 }
 
-bool HttpResponse::file_exists(std::string path)
+bool
+HttpResponse::file_exists(std::string path)
 {
 	std::ifstream file;
-	bool ret = false;
+	bool		  ret = false;
 	file.open(path.c_str());
-	if (file) {
+	if (file)
+	{
 		ret = true;
 		file.close();
 	}
 	return ret;
 }
 
-void HttpResponse::set_response_type(std::string path)
+void
+HttpResponse::set_response_type(std::string path)
 {
 	std::string type = "";
 	type = path.substr(path.rfind(".") + 1, path.size() - path.rfind("."));
@@ -79,11 +111,14 @@ void HttpResponse::set_response_type(std::string path)
 		_response_map["Content-Type"] = "image/png";
 	else if (type == "bmp")
 		_response_map["Content-Type"] = "image/bmp";
+	else if (type == "ico")
+		_response_map["Content-Type"] = "image/x-icon";
 	else
 		_response_map["Content-Type"] = "text/plain";
 }
 
-void HttpResponse::print_response_map(void)
+void
+HttpResponse::print_response_map(void)
 {
 	std::map<std::string, std::string>::iterator it;
 	for (it = _response_map.begin(); it != _response_map.end(); it++)
@@ -92,22 +127,14 @@ void HttpResponse::print_response_map(void)
 	}
 }
 
-
-void HttpResponse::response_handler(void)
-{
-	init_response_map();
-	// CHECKER HERE
-	// 200 OK for now
-	load_response_map(200);
-}
-
-void HttpResponse::construct_header_string(void)
+void
+HttpResponse::construct_header_string(void)
 {
 	std::string CRLF = "\r\n";
 
 	_response_map["header-string"] += _response_map["Status-line"];
 	_response_map["header-string"] += CRLF;
-	_response_map["header-string"] += "Date: " +_response_map["Date"];
+	_response_map["header-string"] += "Date: " + _response_map["Date"];
 	_response_map["header-string"] += CRLF;
 	_response_map["header-string"] += "Server: " + _response_map["Server"];
 	_response_map["header-string"] += CRLF;
@@ -120,31 +147,49 @@ void HttpResponse::construct_header_string(void)
 	_response_map["header-string"] += CRLF;
 }
 
-void HttpResponse::construct_body_string(std::string path_to_file)
+void
+HttpResponse::construct_body_string(std::string path_to_file)
 {
-	std::ifstream file;
+	std::ifstream	  file;
 	std::stringstream buffer;
 
 	file.open(path_to_file.c_str());
-	if (file.fail()) {
+	if (file.fail())
+	{
 		std::cout << "Open file error" << std::endl;
 		return;
 	}
-	buffer <<  file.rdbuf();
+	buffer << file.rdbuf();
 	_response_map["body-string"] = buffer.str();
 	file.close();
 }
 
-void HttpResponse::construct_full_response(void)
+void
+HttpResponse::construct_full_response(void)
 {
 	_response_map["full-response-string"] += _response_map["header-string"];
 	_response_map["full-response-string"] += _response_map["body-string"];
 }
 
-
-std::string HttpResponse::get_http_response(void)
+std::string
+HttpResponse::get_http_response(void)
 {
 	std::string ret;
-	ret  = _response_map["full-response-string"];
+	ret = _response_map["full-response-string"];
 	return ret;
+}
+
+void
+HttpResponse::create_error_html_page(int code)
+{
+	std::string html_page = "";
+	html_page += "<!DOCTYPE html><html><head><link rel=\"stylesheet\"href=\"style.css\"/><link rel=\"icon\" "
+				 "href=\"favicon.ico\"><title>";
+	html_page += std::to_string(code);
+	html_page
+		+= "</title></head><body><div class=\" wrapper\"><div class=\"centered-box\"><h1 class=\"title\">";
+	html_page += _status_code.get_key_value_formated(code);
+	html_page += "</h1></div></div></body></html>";
+	_response_map["body-string"] = html_page;
+	std::cout << "AUTO_HTML PAGE:\n" << html_page << std::endl;
 }
