54c1b62f78570a148deb5132415b08b970bee905
Robin Burri <robinburri@tutanota.com>
2023-03-14 11:09:31 +0100

    created operator<< for HttpResponse +  clean up

--- inc/httpResponse.hpp
+++ inc/httpResponse.hpp
@@ -10,28 +10,31 @@
 
 class HttpResponse
 {
-public:
+  public:
+	typedef std::map<std::string, std::string> t_object;
+	t_object								   _response_map;
+
 	HttpResponse();
-	virtual ~HttpResponse(void);
+	~HttpResponse(void);
 	std::string get_http_response(void);
-	void print_response_map(void);
-	void load_http_request(HttpRequest &req);
+	void		load_http_request(HttpRequest &req);
 
-	private : std::map<std::string, std::string> _response_map;
-	StatusCode _status_code;
+  private:
+	StatusCode	_status_code;
 	HttpRequest _request;
-	std::string _request_path;
 
-	void load_content_length(std::string str);
-	void init_response_map(void);
-	void set_response_type(std::string path);
-	bool file_exists(std::string path);
-	void load_response_map(int status_code);
+	void		set_content_length(std::string str);
+	void		init_response_map(void);
+	void		set_response_type(std::string path);
+	bool		file_exists(std::string path);
+	void		load_response_map(int status_code);
 	std::string get_time_stamp(void);
-	void construct_header_string(void);
-	void construct_body_string(std::string path_to_file);
-	void construct_full_response(void);
-	void create_error_html_page(int code);
+	void		construct_header_string(void);
+	void		construct_body_string(std::string path_to_file);
+	void		construct_full_response(void);
+	void		create_error_html_page(int code);
 };
+
+std::ostream &operator<<(std::ostream &, HttpResponse const &);
 
 #endif
