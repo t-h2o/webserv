017c50367b3f0f3f42d8961c00876fd7cba0a3c5
|°_°| <grivel.theo@protonmail.com>
2023-03-14 23:09:45 +0100

    new class: File

---
+++ inc/File.hpp
@@ -0,0 +1,29 @@
+#ifndef FILE_HPP
+#define FILE_HPP
+
+#include <fstream>
+#include <iostream>
+#include <stdexcept>
+
+/* This class open a file at the construction of an instance
+ * and close it at the desctruction.
+ *
+ * The getc() member function return a character.
+ * getc() do not return any blank character.
+ */
+
+class File
+{
+  public:
+	File(const char *);
+	~File(void);
+
+	char getc(void);
+
+  private:
+	std::string	 _current_line;
+	std::fstream _file;
+	size_t		 _index;
+};
+
+#endif /* FILE_HPP */
---
+++ src/File/File.cpp
@@ -0,0 +1,26 @@
+#include "File.hpp"
+
+File::File(const char *path_file) : _index(0)
+{
+	_file.open(path_file, std::fstream::in);
+
+	/* Check if the file is valid */
+	if (_file.is_open() == 0)
+		throw std::runtime_error("Error: read(): file is not valid");
+
+	getline(_file, _current_line);
+}
+
+File::~File(void) { _file.close(); }
+
+char
+File::getc(void)
+{
+	if (_current_line[_index] == 0)
+	{
+		getline(_file, _current_line);
+		_index = 0;
+	}
+
+	return _current_line[_index];
+}
