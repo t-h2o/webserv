3d7e9d15d907aea85fa23b66412eaa755f123577
Theo Grivel <tgrivel@c1r16s7.42lausanne.ch>
2023-03-16 12:36:03 +0100

    tdd: file: update test

--- test/src/File/02_test.cpp
+++ test/src/File/02_test.cpp
@@ -1,17 +1,25 @@
 #include "File.hpp"
 
-int
-test_file_all_file(void)
+static int
+test_a_file(const char *path_file, std::string expected_output)
 {
-	File file("test/src/File/file.txt");
+	File file(path_file);
 
 	std::string all_file;
 
 	for (char c = file.getc(); c; c = file.getc())
 		all_file += c;
 
-	if (all_file != "Hello World !I very like C++98......IMAO")
+	if (all_file != expected_output)
 		return 1;
+	return 0;
+}
+
+int
+test_file_all_file(void)
+{
+	test_a_file("test/src/File/config.json", "{\"firstName\":\"John\"}");
+	test_a_file("test/src/File/file.txt", "HelloWorld!IverylikeC++98......IMAO");
 
 	return 0;
 }
