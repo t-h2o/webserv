1e69f1724cbefe7a3a5e6d0a11957437b5d2dbbe
hack10101101 <kevin.dinocera@protonmail.com>
2023-03-24 15:33:56 +0100

    add process creat and read file

--- src/cgi/execve.cpp
+++ src/cgi/execve.cpp
@@ -16,8 +16,8 @@
 int
 execution_cgi(char **envp)
 {
-	char	args[6];
 	int	 	pipefd[2];
+	char	array[11];
 	(void) envp;
 
 	if (pipe(pipefd) == -1)
@@ -25,6 +25,7 @@ execution_cgi(char **envp)
 		std::cerr << "error pipe" << std::endl;
 		return (1);
 	}
+
 	pid_t pid = fork();
 
 	if (pid == -1)
@@ -35,20 +36,34 @@ execution_cgi(char **envp)
 	else if (pid == 0)
 	{
 		// child process
+		std::cout << "child" << std::endl;
+		std::cout << pipefd[1] << std::endl;
+		std::ofstream infile("test.txt");
+
+		infile << "hello world";
+
 		close(pipefd[0]);
-		dup2(pipefd[1], STDOUT_FILENO);
-		write(pipefd[1], "hello", 5);
+		infile.close();
 		close(pipefd[1]);
 	}
 
 	else
 	{
 		// parent process
+		std::cout << "parent" << std::endl;
+		std::cout << pipefd[0] << std::endl;
 		close(pipefd[1]);
-		dup2(pipefd[0], STDIN_FILENO);
-		read(pipefd[0], args, 5);
-		printf("array = %s\n", args);
+		int nbytes = read(pipefd[0], array, sizeof(array));
+		array[nbytes] = '\0';
+		std::cout << array;
+
 		close(pipefd[0]);
+
+		std::ifstream outfile("test.txt");
+		std::string line;
+		while (std::getline(outfile, line))
+			std::cout << "parent process readL: " << line << std::endl;
+		outfile.close();
 	}
 
 return (0);
