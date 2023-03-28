fbc773a03a59fc04c48aa3e6fdc0dfcb824b1a37
hack10101101 <kevin.dinocera@protonmail.com>
2023-03-28 15:14:31 +0200

    remove commentary php file

--- test.php
+++ test.php
@@ -1,11 +1,3 @@
 <?php
-phpinfo();
+    phpinfo();
 ?>
-<!-- ob_start(); // Mise en memoire buffer de sortie -->
-<!-- $info = ob_get_contents(); // Recuperation de la sortie mise en memoire tampon -->
-<!-- ob_end_clean(); // Effacement de la memoire tampon -->
-
-<!-- $file = 'info.html'; -->
-<!-- file_put_contents($file, $info); // Ecriture du contenu dans le fichier -->
-
-<!-- echo "le contenu de phpinfo() a été copié dans $file"; -->
