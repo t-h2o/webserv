540b549da84b22002cc932f973031a2e76fadf88
Theo Grivel <tgrivel@c1r15s7.42lausanne.ch>
2023-04-20 14:34:24 +0200

    tdd: json::File empty string

--- test/src/File/00_launcher.cpp
+++ test/src/File/00_launcher.cpp
@@ -11,5 +11,6 @@ launch_file(void)
 	list.load_test("not valid file", &test_file_not_valid_file);
 	list.load_test("get string", &test_file_get_string);
 	list.load_test("get number", &test_file_get_number);
+	list.load_test("get string: empty string", &test_file_get_string_empty_string);
 	return (list.launch_tests());
 }
