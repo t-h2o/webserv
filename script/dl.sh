1acee742a05d6d22067cfd265ebde983677a2b75
Theo Grivel <tgrivel@c1r15s7.42lausanne.ch>
2023-04-21 17:13:36 +0200

    seems finish

--- script/dl.sh
+++ script/dl.sh
@@ -2,52 +2,75 @@
 
 PATH_PHP=$HOME/goinfre/php
 
-#tar xvzf v2.10.4.tar.gz
-#tar xv php-8.2.5.tar.gz
-#tar xvzf autoconf-2.10.tar.gz
-#tar xvzf automake-1.16.tar.gz
-#
-#
-######### libxml2
-#
-#curl -LO https://download.gnome.org/sources/libxml2/2.9/libxml2-2.9.9.tar.xz
-#tar xvzf libxml2-2.9.9.tar.xz
-#cd libxml2-2.9.9
-#./configure
-#make
-#
-## path to find the dynamic library
-#export PKG_CONFIG_PATH=$PWD:$PKG_CONFIG_PATH
-#
-########
-#
-#curl -LO https://www.sqlite.org/2023/sqlite-tools-osx-x86-3410200.zip
-#unzip sqlite-tools-osx-x86-3410200.zip
-#
-#export PATH=$PWD/sqlite-tools-osx-x86-3410200/:$PATH
-
 # $1 : link
+# $2 : extension
 download_handle () {
-	#curl -LO $1
 	FILE_DOWNLOADED=$(basename $1)
+	FOLDER_CREATED=$(basename -s $2 $FILE_DOWNLOADED)
+	if [ -d $FOLDER_CREATED ] ; then
+		printf "folder \"%s\" already exist\n" "$FOLDER_CREATED"
+		return 0
+	fi
+
+	curl -LO $1
 	printf "Download %s\n" "$FILE_DOWNLOADED"
-	if ! tar xvzf ${FILE_DOWNLOADED} >/dev/null ; then
-		printf "do not find\n"
+	if ! tar xvzf ${FILE_DOWNLOADED} 2>/dev/null ; then
+		printf "do not find \"%s\"\n" "$FILE_DOWNLOADED"
 		return 0
 	fi
 	rm ${FILE_DOWNLOADED}
+}
+
+download_sources () {
+
+	download_handle "https://www.php.net/distributions/php-8.2.5.tar.gz" ".tar.gz"
+	download_handle "https://download.gnome.org/sources/libxml2/2.9/libxml2-2.9.9.tar.xz" ".tar.xz"
+	download_handle "https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.17.tar.gz" ".tar.xz"
+	download_handle	"https://www.sqlite.org/src/tarball/sqlite.tar.gz" ".tar.gz"
+
+}
+
+compilate_libxml2 () {
+	cd libxml2-2.9.9
+	./configure
+	make
+
+	export PKG_CONFIG_PATH=$PWD:$PKG_CONFIG_PATH
+	cd ..
+}
+
+compilate_php () {
+	cd php-8.2.5
+	./configure --without-iconv
+}
+
+compilate_sqlite3 () {
+	cd sqlite
+	./configure
+	make
+
+	export PKG_CONFIG_PATH=$PWD:$PKG_CONFIG_PATH
+	cd ..
+}
+
+compilate_libiconv () {
+	cd libiconv-1.17
+	./configure --prefix=$PWD
+	make
+
+	export PKG_CONFIG_PATH=$PWD:$PKG_CONFIG_PATH
+	export ICONV_DIR=$PWD
+	cd ..
 }
 
 main () {
 	mkdir -p $PATH_PHP
 	cd $PATH_PHP
 
-	#download_handle "https://www.php.net/distributions/php-8.2.5.tar.gz"
-	download_handle "https://download.gnome.org/sources/libxml2/2.9/libxml2-2.9.9.tar.xz"
-
-#curl -LO https://github.com/GNOME/libxml2/archive/refs/tags/v2.10.4.tar.gz
-#curl -LO http://ftp.gnu.org/gnu/autoconf/autoconf-latest.tar.gz
-#curl -LO https://ftp.gnu.org/gnu/automake/automake-1.16.tar.gz
+	download_sources
+	compilate_libxml2
+	compilate_sqlite3
+	compilate_php
 }
 
 main
