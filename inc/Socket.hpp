8325a96e8de645989c4bada90332ccb5bfcc79b8
Robin Burri <robinburri@tutanota.com>
2023-04-24 14:54:13 +0200

    refactoring Socket::get_port() deleted

--- inc/Socket.hpp
+++ inc/Socket.hpp
@@ -19,35 +19,34 @@
 class Socket
 {
   private:
+	int				   _connection_fd;
 	int				   _sock_id;
 	int				   _connection;
+	std::string		   _dir_path;
 	struct sockaddr_in _address;
 	void			   create_socket(int domain, int type, int protocol);
 	void			   binding_socket();
 	void			   start_listening();
 	void			   test_socket(int item_to_test, const char *);
 	void			   set_socket_non_blocking();
+	void			   multipart_handler(int);
+	void			   delete_handler();
+	void			   clean_request();
+	void			   send_response();
+	std::string		   get_file_name(std::string);
+	void			   create_new_file(std::string);
+	std::string		   clean_end_of_file(std::string str_to_clean);
+	std::string		   get_dir_path() const;
 
   public:
-	int			   _connection_fd;
 	http::Request  request;
 	http::Response response;
 	std::string	   header_str;
 	std::string	   body_str;
-	std::string	   dir_path;
 	Socket(int domain, unsigned short port, int type, int protocol, std::string path);
-	int			   socket_recv();
-	void		   socket_accept();
-	int			   get_sock_id() const;
-	unsigned short get_port() const;
-	std::string	   get_file_name(std::string);
-	void		   create_new_file(std::string);
-	std::string	   clean_end_of_file(std::string str_to_clean);
-	std::string	   get_dir_path() const;
-	void		   multipart_handler(int);
-	void		   delete_handler();
-	void		   clean_request();
-	void		   send_response();
+	int	 socket_recv();
+	void socket_accept();
+	int	 get_sock_id() const;
 };
 
 #endif
