719d88e8d94984a23837ab0eb3e64ad6e9ec3c4a
|°_°| <grivel.theo@protonmail.com>
2023-04-27 13:46:34 +0200

    script to build php-cgi (#40)
    1) download:
    - libxml2
    - sqlite3
    - php8
    
    2) compilate
    - libxml2
    - sqlite3
    - php8
    
    libxml2 and sqlite3 are download for the
    42 macOS
    
    resource:
    https://www.shellcheck.net/

---
+++ script/dl.sh
@@ -0,0 +1,129 @@
+#!/bin/sh
+
+PATH_PHP="${HOME}"/goinfre/php
+
+# $1 : message
+# $2 : color
+print_big_message () {
+	if [ -z "${2}" ]; then
+		printf ""
+	elif [ "${2}" = "red" ]; then
+		printf "%b" "\033[31;1m"
+	elif [ "${2}" = "green" ]; then
+		printf "%b" "\033[32;1m"
+	elif [ "${2}" = "yellow" ]; then
+		printf "%b" "\033[33;1m"
+	fi
+
+	printf "################################################################################\n"
+	printf "# %s\n" "${1}"
+	printf "################################################################################\n\n"
+	printf "%b" "\033[0m"
+}
+
+# $1 exit code
+success_failure () {
+if [ "${1}" -eq 0 ] ; then
+	print_big_message "SUCCESS" "green"
+else
+	print_big_message "FAILURE" "red"
+	exit 1
+fi
+}
+
+# $1 : link
+# $2 : extension
+download_handle () {
+	FILE_DOWNLOADED=$(basename "$1")
+	FOLDER_CREATED=$(basename -s "$2" "${FILE_DOWNLOADED}")
+	if [ -d "${FOLDER_CREATED}" ] ; then
+		print_big_message "folder \"${FOLDER_CREATED}\" already exist" "yellow"
+		return 0
+	fi
+
+	curl -LO "$1"
+	print_big_message "curl -LO ${1}"
+	printf "Download %s\n" "${FILE_DOWNLOADED}"
+	if ! tar xvzf "${FILE_DOWNLOADED}" 2>/dev/null ; then
+		print_big_message "do not find \"${FILE_DOWNLOADED}\"\n" "red"
+		exit 1
+		return 0
+	fi
+	rm "${FILE_DOWNLOADED}"
+}
+
+download_sources () {
+
+	download_handle "https://www.php.net/distributions/php-8.2.5.tar.gz" ".tar.gz"
+	download_handle "https://download.gnome.org/sources/libxml2/2.9/libxml2-2.9.9.tar.xz" ".tar.xz"
+	download_handle	"https://www.sqlite.org/src/tarball/sqlite.tar.gz" ".tar.gz"
+
+}
+
+compilate_libxml2 () {
+	cd libxml2-2.9.9 || exit
+	print_big_message "./configure"
+	./configure
+	success_failure $?
+
+	print_big_message "make"
+	make
+	success_failure $?
+
+	export PKG_CONFIG_PATH="${PWD}":"${PKG_CONFIG_PATH}"
+	print_big_message "PKG_CONFIG_PATH = ${PKG_CONFIG_PATH}"
+	cd .. || exit
+}
+
+compilate_php () {
+	cd php-8.2.5 || exit
+	print_big_message "./configure --without-iconv"
+	./configure --without-iconv
+	success_failure $?
+
+	print_big_message "make"
+	make
+	success_failure $?
+}
+
+compilate_sqlite3 () {
+	cd sqlite || exit
+	print_big_message "./configure"
+	./configure
+	success_failure $?
+
+	print_big_message "make"
+	make
+	success_failure $?
+
+	export PKG_CONFIG_PATH="${PWD}":"${PKG_CONFIG_PATH}"
+	print_big_message "PKG_CONFIG_PATH = ${PKG_CONFIG_PATH}"
+	cd .. || exit
+}
+
+main () {
+	mkdir -p "${PATH_PHP}"
+	cd "${PATH_PHP}" || exit
+
+	print_big_message "Download source files" "green"
+	download_sources
+
+	print_big_message "Compilate libxml2" "green"
+	compilate_libxml2
+
+	print_big_message "Compilate sqlite3" "green"
+	compilate_sqlite3
+
+	print_big_message "Compilate php8" "green"
+	compilate_php
+
+	if ls "${PATH_PHP}/php-8.2.5/sapi/cgi/php-cgi" ; then
+		print_big_message "SUCCESS: cgi in ${PATH_PHP}/php-8.2.5/sapi/cgi/php-cgi" "green"
+	else
+		print_big_message "FAILURE: cgi in ${PATH_PHP}/php-8.2.5/sapi/cgi/php-cgi" "red"
+	fi
+}
+
+
+print_big_message "launch the main()" "green"
+main
