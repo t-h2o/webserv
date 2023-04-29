282a510794d02cf27f4279fc090981b51624295b
kdi-noce <kevin.dinocera@protonmail.com>
2023-04-29 14:54:22 +0200

    Update src/cgi/execve.cpp
    Co-authored-by: |°_°| <grivel.theo@protonmail.com>

--- src/cgi/execve.cpp
+++ src/cgi/execve.cpp
@@ -33,7 +33,7 @@ CGI::set_env(char *args)
 //		// en charge).
 //		_env["AUTH_TYPE"] = "null";
 //		// Donne le nombre d'octets transmis par le client.
-//		_env["CONTENT_LENGTH"] = std98::to_string(1000);
+//		_env["CONTENT_LENGTH"] = "1000";
 //		// Donne le type de donnée transmise par le client si l'attribut METHOD a pour valeur POST.
 //		_env["CONTENT_TYPE"] = "application/x-www-form-urlencoded";
 //		// Donne la version du CGI utilisé.
