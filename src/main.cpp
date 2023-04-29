d26cc59c292ad4d0c1824d762a79699d32c58c22
hack10101101 <kevin.dinocera@protonmail.com>
2023-04-29 15:06:41 +0200

    restore main

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,9 @@
-#include "../inc/cgi.hpp"
 #include "arguments.hpp"
 #include "webserver.hpp"
 
 int
 main(int argc, char **argv)
 {
-	char *argsss[3];
-
-	argsss[0] = (char *)"./bin/php-cgi";
-	argsss[1] = (char *)"test/src/cgi/test.php";
-	argsss[2] = 0;
-
-	std::vector<char *> popol;
-
-	popol.push_back(argsss[0]);
-	popol.push_back(argsss[1]);
-	popol.push_back(argsss[2]);
-
-	execve(popol[0], &(popol[0]), 0);
-	return 0;
-
 	if (arguments::check_argc_number(argc))
 		return 1;
 
