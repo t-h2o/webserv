62090067febd7ff217b79d55426b6c30a6961244
hack10101101 <kevin.dinocera@protonmail.com>
2023-05-02 12:37:06 +0200

    remove commentare

--- test/src/cgi/02_test.cpp
+++ test/src/cgi/02_test.cpp
@@ -8,7 +8,6 @@ test_execve_cgi(void)
 	std::string output;
 	char	   *argument[4];
 	{
-		std::cout << "test4" << std::endl;
 		argument[0] = (char *)"bin/php-cgi";
 		argument[1] = (char *)"test/src/cgi/test.php";
 		argument[2] = (char *)"";
@@ -16,12 +15,10 @@ test_execve_cgi(void)
 
 		CGI exec(argument[0], argument[1], argument[2]);
 		output = exec.execution_cgi(argument[1]);
-		std::cout << "output hello: " << output << std::endl;
 		if (output.find("hello World!") == std::string::npos)
 			return (1);
 	}
 	{
-		std::cout << "test5" << std::endl;
 		argument[0] = (char *)"bin/php-cgi";
 		argument[1] = (char *)"test/src/cgi/test3.php";
 		argument[2] = (char *)"";
@@ -29,7 +26,6 @@ test_execve_cgi(void)
 
 		CGI exec(argument[0], argument[1], argument[2]);
 		output = exec.execution_cgi(argument[1]);
-		std::cout << "output hello: " << output << std::endl;
 		if (output.find("My username is kdi-noce!") == std::string::npos)
 		{
 			std::cout << "in 5" << std::endl;
--- test/src/cgi/03_test.cpp
+++ test/src/cgi/03_test.cpp
@@ -8,7 +8,6 @@ test_execve_cgi_env(void)
 	std::string output;
 	char	   *arguments[4];
 	{
-		std::cout << "test6" << std::endl;
 		arguments[0] = (char *)"bin/php-cgi";
 		arguments[1] = (char *)"test/src/cgi/env.php";
 		arguments[2] = (char *)"";
@@ -16,12 +15,10 @@ test_execve_cgi_env(void)
 
 		CGI exec(arguments[0], arguments[1], arguments[2]);
 		output = exec.execution_cgi(arguments[1]);
-		std::cout << "output hello: " << output << std::endl;
 		if (output.find("failed.") != std::string::npos)
 			return (1);
 	}
 	{
-		std::cout << "test7" << std::endl;
 		arguments[0] = (char *)"bin/php-cgi";
 		arguments[1] = (char *)"test/src/cgi/input.php";
 		arguments[2] = (char *)"";
@@ -29,7 +26,6 @@ test_execve_cgi_env(void)
 
 		CGI exec(arguments[0], arguments[1], arguments[2]);
 		output = exec.execution_cgi(arguments[1]);
-		std::cout << "output hello: " << output << std::endl;
 		if (output.find("?php") != std::string::npos)
 			return (1);
 	}
