5e42dc11cab86d27a436e1c0f1fcc5aab8ca8d6f
Robin Burri <robinburri@tutanota.com>
2023-05-02 13:55:22 +0200

    Just for you, Theo

--- src/http/Response.cpp
+++ src/http/Response.cpp
@@ -221,7 +221,7 @@ Response::has_php_extension(const Request &request) const
 {
 	std::string path = request.get_path();
 	size_t		last_dot = path.find_last_of('.');
-	std::string extenstion = path.substr(last_dot);
+	std::string extenstion(path.substr(last_dot));
 	return (extenstion.compare(".php") == 0);
 }
 
