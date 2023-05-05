c7c04a3f8856308a5581675ba6caaa4b185e4e82
Theo Grivel <tgrivel@c1r16s7.42lausanne.ch>
2023-05-05 13:35:07 +0200

    (void) _server_config

--- src/http/Response.cpp
+++ src/http/Response.cpp
@@ -5,7 +5,7 @@ namespace http
 
 StatusCode Response::_status_code;
 
-Response::Response(const json::Value &server_config) : server_name("Webserver"), _server_config(server_config) {}
+Response::Response(const json::Value &server_config) : server_name("Webserver"), _server_config(server_config) {(void) _server_config;}
 
 Response::~Response(void) {}
 
