85c804776fca78c8755bb8b4dbdaf902515a1c9e
hack10101101 <kevin.dinocera@protonmail.com>
2023-05-05 18:23:38 +0200

    add new test

--- test/src/php/00_launcher.cpp
+++ test/src/php/00_launcher.cpp
@@ -9,5 +9,6 @@ launch_php(void)
 	list.load_test("check_webs_output_php", &test_php_output);
 	list.load_test("check_cgi_php", &test_cgi_with_php);
 	list.load_test("check_cgi_php", &test_cgi_with_php_query);
+	list.load_test("check_cgi_php", &test_cgi_with_php_query2);
 	return (list.launch_tests());
 }
