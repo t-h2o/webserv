e9998235269aa7d0133263e1d78d99f12b77f324
Theo Grivel <grivel.theo@protonmail.com>
2023-05-08 16:40:55 +0200

    update script

--- python/script.py
+++ python/script.py
@@ -4,16 +4,19 @@ import requests
 
 def request_get (url, expected_status_code):
     print("Request get on: " + url)
-    print("method GET on " + url)
+
     request_index = requests.get(url)
+
     if request_index.status_code != expected_status_code:
         print ("error")
     else:
         print ("good")
+    print()
 
 def main ():
     request_get("http://webserv.com:6060/index.html", 200)
     request_get("http://webserv.com:6060/index.html", 200)
+    request_get("http://webserv.com:6060/bad_page", 404)
 
 if __name__ == "__main__":
     main()
