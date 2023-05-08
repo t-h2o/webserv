73a32e167b7649cb30a6a2cfdc2e53ed643b0a4a
Robin Burri <robinburri@tutanota.com>
2023-05-08 10:16:08 +0200

    changed: config.json first server data to make it work

--- src/serverTest/serverTest.cpp
+++ src/serverTest/serverTest.cpp
@@ -29,13 +29,12 @@ serverTest(json::t_object *config)
 	 */
 	json::Value val(config);
 	json::Value server_config;
-	server_config.duplicate(val.get("foo.com").get<json::t_object>());
+	server_config.duplicate(val.get("nicesite.com").get<json::t_object>());
 	std::cout << server_config << std::endl;
 	unsigned short port = server_config.get("port").get<double>();
 	Socket		   sock(AF_INET, port, SOCK_STREAM, 0, server_config);
 
-	return;
-	create_upload_folder(val);
+	create_upload_folder(server_config);
 	while (1)
 	{
 		std::cout << "++++++ Waiting for new connection ++++++" << std::endl;
