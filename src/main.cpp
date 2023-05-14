00314d90861a3dc1c621e26408a2e0c770252434
|°_°| <grivel.theo@protonmail.com>
2023-05-14 10:36:00 +0200

    default configuration file
    Votre programme doit prendre un fichier de
    configuration en argument ou utiliser un
    chemin par défaut

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,26 @@
 #include "arguments.hpp"
 #include "webserver.hpp"
 
+#define DEFAULT_CONFIG "./config.json"
+
 int
 main(int argc, char **argv)
 {
 	if (arguments::check_argc_number(argc))
 		return 1;
 
-	if (arguments::check_argv_access(argv[1]))
-		return 1;
+	if (argc == 2)
+	{
+		if (arguments::check_argv_access(argv[1]))
+			return 1;
 
-	return webserver(argv[1]);
+		return webserver(argv[1]);
+	}
+	else if (argc == 1)
+	{
+		if (arguments::check_argv_access(DEFAULT_CONFIG))
+			return 1;
+
+		return webserver(DEFAULT_CONFIG);
+	}
 }
